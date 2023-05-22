8d5ef124f752b7a270ddbeef268d0fedab57586d
Moritz Lindner <lindner197@gmail.com>
2023-05-22 14:11:17 +0200

    create simple webpage to call contract

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import lorenIpsumContract from './contracts/StringContract.json';
+import Web3 from 'web3';
+
+
+const contractABI = lorenIpsumContract.abi;
+const contractAddress = '0xD174e0f4c2FaD1cae33eE5C0aA9B014421C519f2'; // Replace with your contract address
+
+const web3 = new Web3(Web3.givenProvider);
+// @ts-ignore
+const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [value, setValue] = useState<String>()
+
+    contractInstance.methods.getString().call()
+        .then((result: any) => {
+            setValue(result)
+        })
+        .catch((error: any) => {
+            setValue("error: ${error}")
+            console.error(error);
+        });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Value from contract: {value}
+                </p>
+                <h1>{value}</h1>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
