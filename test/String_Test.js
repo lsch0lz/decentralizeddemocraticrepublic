77c2f3af5121379962fb8ed3de333b04d6d028fb
Henry Beiker <93589200+Asrethet@users.noreply.github.com>
2023-05-29 17:43:30 +0200

    grobe struktur für schule - noch zu testen

--- test/String_Test.js
+++ test/String_Test.js
@@ -21,7 +21,7 @@ async function test() {
 
     // Set a new string value
     const accounts = await web3.eth.getAccounts();
-    await deployedContract.methods.setString('\n Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.').send({
+    await deployedContract.methods.setString('\n.').send({
       from: accounts[0],
       gas: 2000000,
     });
