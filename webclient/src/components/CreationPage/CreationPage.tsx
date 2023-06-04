7ea560c9a8980bbf29c4feee984afeaaeaeca832
Moritz Lindner <lindner197@gmail.com>
2023-06-04 18:35:04 +0200

    generate creation component

---
+++ webclient/src/components/CreationPage/CreationPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './CreationPage.css';
+
+interface CreationPageProps {}
+
+const CreationPage: FC<CreationPageProps> = () => (
+  <div className="CreationPage">
+    CreationPage Component
+  </div>
+);
+
+export default CreationPage;
