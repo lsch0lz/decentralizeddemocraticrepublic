7b769daddda5e6df497e9ca5ba519b069a83e039
Moritz Lindner <lindner197@gmail.com>
2023-06-04 19:23:48 +0200

    add basic creation page with selector

--- webclient/src/components/CreationPage/CreationPage.tsx
+++ webclient/src/components/CreationPage/CreationPage.tsx
@@ -1,12 +1,31 @@
-import React, { FC } from 'react';
+import React, {useState} from 'react';
 import './CreationPage.css';
+import Dropdown, {DropdownOption} from "./dropdown/Dropdown";
 
 interface CreationPageProps {}
 
-const CreationPage: FC<CreationPageProps> = () => (
-  <div className="CreationPage">
-    CreationPage Component
-  </div>
-);
+function CreationPage() {
+    const createOptions: DropdownOption[] = [
+        { value: 'School', label: 'School' },
+        { value: 'Class', label: 'Class' },
+        { value: 'Teacher', label: 'Teacher' },
+        { value: 'Student', label: 'Student' },
+        { value: 'Election', label: 'Election' }
+    ];
 
+    const [selectedOption, setSelectedOption] = useState<DropdownOption | undefined>(undefined);
+
+    const handleSelect = (value: string) => {
+        const selected = createOptions.find((option) => option.value === value);
+        setSelectedOption(selected);
+    };
+
+    return (
+        <div>
+            <Dropdown options={createOptions} onSelect={handleSelect} />
+            {/* Other components and content */}
+            {selectedOption && <p>Selected: {selectedOption.label}</p>}
+        </div>
+    );
+}
 export default CreationPage;
