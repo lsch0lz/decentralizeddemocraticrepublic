77dc1c884f794f95e90ee97f4ddf46f1b9320248
Moritz Lindner <lindner197@gmail.com>
2023-06-05 11:45:59 +0200

    add school saving

--- webclient/src/components/CreationPage/creationforms/SchoolCreation.tsx
+++ webclient/src/components/CreationPage/creationforms/SchoolCreation.tsx
@@ -1,5 +1,28 @@
 import React, {useState} from 'react';
 import './SchoolCreation.css';
+import schoolContract from "../../../contracts/School.json";
+import Web3 from "web3";
+
+const contractABI = schoolContract.abi;
+const contractAddress = '0x73542BC7E1925ce59Fa149DdF06111b49D6982FA'; // Replace with your contract address
+
+const ganacheUrl = 'HTTP://127.0.0.1:7545';
+const httpProvider = new Web3.providers.HttpProvider(ganacheUrl);
+const web3 = new Web3(httpProvider);
+
+// @ts-ignore
+const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
+const createSchool = async (name: string) => {
+    try {
+        const accounts = await web3.eth.getAccounts();
+        console.log('accounts:', accounts)
+        const result = await contractInstance.methods.createSchool(name).send({from: accounts[0]});
+        console.log('School created:', result);
+    } catch (error) {
+        console.error('Failed to create school:', error);
+    }
+};
 
 function SchoolCreation() {
     const [schoolName, setSchoolName] = useState('');
@@ -9,8 +32,9 @@ function SchoolCreation() {
     };
 
     const saveToChain = () => {
-        // TODO: Save to chain
-        console.log('Saving to chain! Need to be implemented');
+        createSchool(schoolName).then(() => {
+            console.log('School created');
+        })
     }
 
     return (
