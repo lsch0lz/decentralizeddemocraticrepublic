f800f8785206eba6b40a516c7e726380523f2144
Moritz Lindner <lindner197@gmail.com>
2023-06-13 11:29:13 +0200

    implement conditional rendering for creation page

--- webclient/src/components/CreationPage/CreationPage.tsx
+++ webclient/src/components/CreationPage/CreationPage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import './CreationPage.css';
 import Dropdown, {DropdownOption} from "./dropdown/Dropdown";
 import SchoolCreation from "./creationforms/SchoolCreation";
@@ -6,26 +6,66 @@ import {ClassCreation} from "./creationforms/ClassCreation";
 import {TeacherCreation} from "./creationforms/TeacherCreation";
 import {StudentCreation} from "./creationforms/StudentCreation";
 import {ElectionCreation} from "./creationforms/ElectionCreation";
+import RoleContext from "../RoleContext";
 
 
 function CreationPage() {
-    const createOptions: DropdownOption[] = [
+
+    const {currentRole} = useContext(RoleContext);
+
+    const teacherOptions: String[] = [
+        'Student',
+        'Election'
+    ];
+
+    const studentOptions: String[] = [
+        'Election'
+    ];
+
+    const possibleCreateOptions: DropdownOption[] = [
         {value: 'School', label: 'School'},
         {value: 'Class', label: 'Class'},
         {value: 'Teacher', label: 'Teacher'},
         {value: 'Student', label: 'Student'},
         {value: 'Election', label: 'Election'}
-    ];
+    ].filter((option) => {
+        switch (currentRole) {
+            case 'Principal':
+                return true;
+            case 'Teacher':
+                return teacherOptions.includes(option.value);
+            case 'Student':
+                return studentOptions.includes(option.value);
+            default:
+                return false;
+        }
+    })
 
-    const [selectedOption, setSelectedOption] = useState<DropdownOption | undefined>(createOptions[0]);
+    const [selectedOption, setSelectedOption] = useState<DropdownOption | undefined>(possibleCreateOptions[0]);
 
     const handleSelect = (value: string) => {
-        const selected = createOptions.find((option) => option.value === value);
+        const selected = possibleCreateOptions.find((option) => option.value === value);
         setSelectedOption(selected);
     };
 
+    function isUserLoggedIn() {
+        return currentRole !== null;
+    }
+
+    function PossibleSelections() {
+        return (
+            <div>
+                <Dropdown options={possibleCreateOptions} onSelect={handleSelect}/>
+                {renderSelectedOptionContent()}
+            </div>
+        );
+    }
+
+    function SignInInfoMessage() {
+        return <h2>You need to be signed in to create something</h2>;
+    }
+
     const renderSelectedOptionContent = () => {
-        // TODO: Filter options based on user role
         switch (selectedOption?.value) {
             case 'School':
                 return <SchoolCreation/>;
@@ -42,12 +82,12 @@ function CreationPage() {
         }
     };
 
-    return (
-        <div>
-            <Dropdown options={createOptions} onSelect={handleSelect} />
-            {renderSelectedOptionContent()}
-        </div>
-    );
+
+    if (!isUserLoggedIn()) {
+        return <SignInInfoMessage/>
+    } else {
+        return PossibleSelections()
+    }
 }
 
 export default CreationPage;
