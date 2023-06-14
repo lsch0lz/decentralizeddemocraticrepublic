0e78fea4bc4498b430f08e1a0f77100527a45c3f
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2023-06-14 16:56:38 +0200

    A pathetic attempt to restructure the code to get rid of the IDs

--- contracts/Schools.sol
+++ contracts/Schools.sol
@@ -1,96 +1,270 @@
 pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2; // Enable string[]
 
 contract School {
-    
     struct Teacher {
         string name;
-        uint256 id;
     }
-    
+
     struct Student {
         string name;
         string password;
         uint256 id;
     }
-    
+
     struct Class {
-        string name;
-        Teacher[] teachers;
-        Student[] students;
+        mapping(uint256 => Teacher) teachers;
+        uint256 teacherCount;
+        mapping(uint256 => Student) students;
+        uint256 studentsCount;
     }
-    
+
     struct SchoolData {
         string name;
         address principal;
-        mapping(uint256 => Class) classes;
+        mapping(bytes32 => Class) classes;
+        bytes32[] classesKeys;
     }
-    
-    mapping(address => SchoolData) public schools;  // Mapping to store school data (Principal is owner)
 
+    mapping(address => SchoolData) public schools;
 
-    // CREATE
     function createSchool(string memory _name) public {
-        require(schools[msg.sender].principal == address(0), "This address already has a school assigned to it");  // Check if school already exists ==> Nein, implizit nur eine Schule pro Adresse (Principal) erlaubt!
-        schools[msg.sender].name = _name;  // Set the school's name
-        schools[msg.sender].principal = msg.sender;  // Set the caller's address as the principal
-    }
-    
-    function createClass(uint256 _classId, string memory _name) public {
-        SchoolData storage school = schools[msg.sender];
-        require(school.principal != address(0), "School does not exist");  // Check if the school exists
-        require(school.classes[_classId].teachers.length == 0, "Class already exists");  // Check if class already exists
-        school.classes[_classId].name = _name;  // Set the class name
-    }
-    
-    function addTeacherToClass(uint256 _classId, string memory _teacherName, uint256 _teacherId) public {
-        SchoolData storage school = schools[msg.sender];
-        require(school.principal != address(0), "School does not exist");  // Check if the school exists
-        require(school.classes[_classId].teachers.length > 0, "Class does not exist");  // Check if class exists
-        school.classes[_classId].teachers.push(Teacher(_teacherName, _teacherId));  // Add teacher to the class
-    }
-    
-    function addStudentToClass(uint256 _classId, string memory _studentName, uint256 _studentId, string memory _password) public {
-        SchoolData storage school = schools[msg.sender];
-        require(school.principal != address(0), "School does not exist");  // Check if the school exists
-        require(school.classes[_classId].teachers.length > 0, "Class does not exist");  // Check if class exists
-        school.classes[_classId].students.push(Student(_studentName, _password, _studentId));  // Add student to the class
+        require(
+            schools[msg.sender].principal == address(0),
+            "This address already has a school assigned to it"
+        );
+        schools[msg.sender].name = _name;
+        schools[msg.sender].principal = msg.sender;
     }
 
+    function createClass(string memory _name) public {
+        SchoolData storage school = schools[msg.sender];
+        require(school.principal != address(0), "School does not exist");
+        require(
+            school.classes[keccak256(bytes(_name))].teacherCount == 0,
+            "Class already exists"
+        );
+        Class memory emptyClass;
+        school.classes[keccak256(bytes(_name))] = emptyClass;
+        school.classesKeys.push(keccak256(bytes(_name)));
+    }
 
-    // READ
+    function addTeacherToClass(
+        string memory _teacherName,
+        string memory _class_name
+    ) public {
+        SchoolData storage school = schools[msg.sender];
+        require(school.principal != address(0), "School does not exist");
+        require(
+            school.classes[keccak256(bytes(_class_name))].teacherCount == 0,
+            "Class does not exist"
+        );
+        Class storage class = school.classes[keccak256(bytes(_class_name))];
+        class.teachers[class.teacherCount] = Teacher(_teacherName);
+        class.teacherCount++;
+    }
+
+    function addStudentToClass(
+        string memory _class_name,
+        string memory _studentName,
+        uint256 _studentId,
+        string memory _password
+    ) public {
+        SchoolData storage school = schools[msg.sender];
+        require(school.principal != address(0), "School does not exist");
+        require(
+            school.classes[keccak256(bytes(_class_name))].teacherCount != 0,
+            "Class does not exist"
+        );
+        Class storage class = school.classes[keccak256(bytes(_class_name))];
+        class.students[class.studentsCount] = Student(
+            _studentName,
+            _password,
+            _studentId
+        );
+        class.studentsCount++;
+    }
+
     function getSchoolName() public view returns (string memory) {
-        return schools[msg.sender].name;  // Get the name of the school
-    }
-    
-    function getClassDetails(uint256 _classId) public view returns (string memory, uint256, uint256) {
-        SchoolData storage school = schools[msg.sender];
-        Class storage class = school.classes[_classId];
-        return (class.name, class.teachers.length, class.students.length);  // Get the class details (name, number of teachers, number of students)
+        return schools[msg.sender].name;
     }
 
-    function log_in(string memory _studentName, string memory _password) public view returns (bool) {
+    function getClassDetails(string memory _class_name)
+    public
+    view
+    returns (uint256, uint256)
+    {
         SchoolData storage school = schools[msg.sender];
-        uint256 classCount = 0;
-        for (uint256 i = 0; i < 1000000; i++) { // Assuming the maximum number of classes is 1,000,000
-            if (school.classes[i].teachers.length > 0) {
-                classCount++;
-            } else {
-                break;
-            }
+        Class storage class = school.classes[keccak256(bytes(_class_name))];
+        return (class.teacherCount, class.studentsCount);
+    }
+
+    function getAllClasses() public view returns (string[] memory) {
+        SchoolData storage school = schools[msg.sender];
+        bytes32[] storage classKeys = school.classesKeys;
+        string[] memory classNames = new string[](classKeys.length);
+
+        for (uint256 i = 0; i < classKeys.length; i++) {
+            classNames[i] = bytes32ToString(classKeys[i]);
         }
 
-        for (uint256 i = 0; i < classCount; i++) {
-            Student[] storage students = school.classes[i].students;
-            for (uint256 j = 0; j < students.length; j++) {
-                if (
-                    keccak256(bytes(students[j].name)) == keccak256(bytes(_studentName)) &&
-                    keccak256(bytes(students[j].password)) == keccak256(bytes(_password))
-                ) {
-                    return true;  // Found the student
-                }
-            }
-        }
+        return classNames;
+    }
 
-        return false;  // Student not found
+    function bytes32ToString(bytes32 _bytes32)
+    private
+    pure
+    returns (string memory)
+    {
+        uint256 length = 0;
+        while (length < 32 && _bytes32[length] != 0) {
+            length++;
+        }
+        bytes memory bytesArray = new bytes(length);
+        for (uint256 i = 0; i < length; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
     }
 }
+
+
+//
+//pragma solidity ^0.5.16;
+//pragma experimental ABIEncoderV2; // Enable string[]
+//
+//contract School {
+//
+//    struct Teacher {
+//        string name;
+//    }
+//
+//    struct Student {
+//        string name;
+//        string password;
+//        uint256 id;
+//    }
+//
+//    struct Class {
+//        mapping(uint256 => Teacher) teachers;
+//        uint256 teacherCount;
+//        // Student[] students; does not work: UnimplementedFeatureError: Copying of type struct School.Student memory[] memory to storage not yet supported.
+//        mapping(uint256 => Student) students;
+//        uint256 studentsCount;
+//    }
+//
+//    struct SchoolData {
+//        string name;
+//        address principal;
+//
+//
+//        mapping(bytes32 => Class) classes;
+//        // It is not trivial to retrieve the keys. Workaround: Store them for easy access
+//        bytes32[] classesKeys;
+//    }
+//
+//    mapping(address => SchoolData) public schools;  // Mapping to store school data (Principal is owner)
+//
+//
+//    // CREATE
+//    function createSchool(string memory _name) public {
+//        require(schools[msg.sender].principal == address(0), "This address already has a school assigned to it");  // Check if school already exists ==> Nein, implizit nur eine Schule pro Adresse (Principal) erlaubt!
+//        schools[msg.sender].name = _name;  // Set the school's name
+//        schools[msg.sender].principal = msg.sender;  // Set the caller's address as the principal
+//    }
+//
+//    function createClass(string memory _name) public {
+//        SchoolData storage school = schools[msg.sender];
+//        require(school.principal != address(0), "School does not exist");  // Check if the school exists
+//        require(school.classes[keccak256(bytes(_name))].teacherCount == 0, "Class already exists");  // Check if class already exists
+//
+//        // Create empty class:
+//        Class memory emptyClass;
+//        school.classes[keccak256(bytes(_name))] = emptyClass;
+//        school.classesKeys.push(keccak256(bytes(_name)));
+//    }
+//
+//    function addTeacherToClass(string memory _teacherName, string memory _class_name) public {
+//        SchoolData storage school = schools[msg.sender];
+//        require(school.principal != address(0), "School does not exist");  // Check if the school exists
+//        require(school.classes[keccak256(bytes(_class_name))].teacherCount == 0, "Class does not exist");  // Check if class exists
+//
+//        // TODO Test if name is unique
+//        Class storage class = school.classes[keccak256(bytes(_class_name))];
+//        class.teachers[class.teacherCount] = Teacher(_teacherName);
+//        class.teacherCount++;
+//    }
+//
+//    function addStudentToClass(string memory _class_name, string memory _studentName, uint256 _studentId, string memory _password) public {
+//        SchoolData storage school = schools[msg.sender];
+//        require(school.principal != address(0), "School does not exist");  // Check if the school exists
+//        require(school.classes[keccak256(bytes(_class_name))].teacherCount == 0, "Class does not exist");  // Check if class exists
+//
+//        Class storage class = school.classes[keccak256(bytes(_class_name))];
+//        class.students[class.studentsCount] = Student(_studentName, _password, _studentId);
+//        class.studentsCount++;
+//    }
+//
+//
+//    // READ
+//    function getSchoolName() public view returns (string memory) {
+//        return schools[msg.sender].name;  // Get the name of the school
+//    }
+//
+//    function getClassDetails(string memory _class_name) public view returns (uint256, uint256) {
+//        SchoolData storage school = schools[msg.sender];
+//        Class storage class = school.classes[keccak256(bytes(_class_name))];
+//        return (class.teacherCount, class.studentsCount);  // Get the class details (number of teachers, number of students)
+//    }
+//
+////    function getAllClasses() public view returns (string[] memory){
+////        SchoolData storage school = schools[msg.sender];
+////
+////        return school.classesKeys;
+////    }
+//
+//    function getAllClasses() public view returns (string[] memory) {
+//        SchoolData storage school = schools[msg.sender];
+//        bytes32[] storage classKeys = school.classesKeys;
+//        string[] memory classNames = new string[](classKeys.length);
+//
+//        for (uint256 i = 0; i < classKeys.length; i++) {
+//            classNames[i] = bytes32ToString(classKeys[i]);
+//        }
+//
+//        return classNames;
+//    }
+//
+//    function bytes32ToString(bytes32 _bytes32) private pure returns (string memory) {
+//        uint256 length = 0;
+//        while (length < 32 && _bytes32[length] != 0) {
+//            length++;
+//        }
+//        bytes memory bytesArray = new bytes(length);
+//        for (uint256 i = 0; i < length; i++) {
+//            bytesArray[i] = _bytes32[i];
+//        }
+//        return string(bytesArray);
+//    }
+//
+//
+//    function log_in(string memory _studentName, string memory _password) public view returns (bool) {
+//        SchoolData storage school = schools[msg.sender];
+//        uint256 classCount = school.classesKeys.length;
+//        // TODO!!!
+//
+////        for (uint256 i = 0; i < classCount; i++) {
+////            Student[] storage students = school.classes[school.classesKeys[i]].students;
+////            for (uint256 j = 0; j < students.length; j++) {
+////                if (
+////                    keccak256(bytes(students[j].name)) == keccak256(bytes(_studentName)) &&
+////                    keccak256(bytes(students[j].password)) == keccak256(bytes(_password))
+////                ) {
+////                    return true;  // Found the student
+////                }
+////            }
+////        }
+//
+//        return false;  // Student not found
+//    }
+//}
