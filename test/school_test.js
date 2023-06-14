9d498c44c3b8e389da018e55eff1bb4131a82e3a
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2023-06-14 11:14:51 +0200

    Simplify tests and make readable

--- test/school_test.js
+++ test/school_test.js
@@ -18,24 +18,18 @@ const SchoolsContract = contract(schoolsContractJson);
 SchoolsContract.setProvider(provider);
 
 async function createSchoolTest(name, fromAccount) {
-  try {
-    const instance = await SchoolsContract.deployed();
-    const result = await instance.createSchool(name, { from: fromAccount });
-    // Code would break here and jump to "catch"
+  const instance = await SchoolsContract.deployed();
+  const result = await instance.createSchool(name, { from: fromAccount });
+  // Code would break here
 
-    console.log('School created successfully!');
-    console.log('Transaction hash:', result.tx);
+  console.log('School created successfully!');
+  console.log('Transaction hash:', result.tx);
 
-    // Check if logs were emitted during the transaction
-    if (result.logs && result.logs.length > 0) {
-      console.log('School address:', result.logs[0].args.principal);
-    } else {
-      console.log('No logs found.');
-    }
-    return true; // Everything worked
-  } catch (error) {
-    console.error('Error creating school:', error);
-    return false; // Oh boy, we got a problem here!
+  // Check if logs were emitted during the transaction
+  if (result.logs && result.logs.length > 0) {
+    console.log('School address:', result.logs[0].args.principal);
+  } else {
+    console.log('No logs found.');
   }
 }
 
@@ -52,23 +46,12 @@ const schoolName = "sdfsdf";
 const account = "0x993D35AB34ac41D6c836f25BB40c3FAB4cEEbecb";
 
 describe('School creating (And Reading) Test', function (){
-  it('Create School', function (){
-    let res = false;
-    createSchoolTest(schoolName, account).then((result) => {
-      res = result;
-    }).catch((error) => {
-      console.error(error);
-    });
-    expect(res).to.be.true;
+  it('Create School', async function (){
+    await createSchoolTest(schoolName, account)
   });
 
-  it('Check for School on Blockchain', function () {
-    let res = 0;
-    getSchoolTest(account).then((result) => {
-      res = result;
-    }).catch((error) => {
-      console.error(error);
-    });
+  it('Check for School on Blockchain', async function () {
+    const res = await getSchoolTest(account);
     expect(res).to.equal(schoolName)
   });
 });
