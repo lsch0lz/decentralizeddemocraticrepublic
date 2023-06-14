2a47690cd81bf2f5e7937fcfb326847f667bede4
Moritz Lindner <lindner197@gmail.com>
2023-06-14 12:04:28 +0200

    add other styling stuff

--- webclient/src/components/CreationPage/CreationPage.tsx
+++ webclient/src/components/CreationPage/CreationPage.tsx
@@ -1,33 +1,26 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import './CreationPage.css';
-import Dropdown, {DropdownOption} from "./dropdown/Dropdown";
-import SchoolCreation from "./creationforms/SchoolCreation";
-import {ClassCreation} from "./creationforms/ClassCreation";
-import {TeacherCreation} from "./creationforms/TeacherCreation";
-import {StudentCreation} from "./creationforms/StudentCreation";
-import {ElectionCreation} from "./creationforms/ElectionCreation";
-import RoleContext, {Role} from "../RoleContext";
-
+import Dropdown, { DropdownOption } from './dropdown/Dropdown';
+import SchoolCreation from './creationforms/SchoolCreation';
+import { ClassCreation } from './creationforms/ClassCreation';
+import { TeacherCreation } from './creationforms/TeacherCreation';
+import { StudentCreation } from './creationforms/StudentCreation';
+import { ElectionCreation } from './creationforms/ElectionCreation';
+import RoleContext, { Role } from '../RoleContext';
 
 function CreationPage() {
+    const { currentRole } = useContext(RoleContext);
 
-    const {currentRole} = useContext(RoleContext);
+    const teacherOptions: string[] = ['Student', 'Election'];
 
-    const teacherOptions: String[] = [
-        'Student',
-        'Election'
-    ];
-
-    const studentOptions: String[] = [
-        'Election'
-    ];
+    const studentOptions: string[] = ['Election'];
 
     const possibleCreateOptions: DropdownOption[] = [
-        {value: 'School', label: 'School'},
-        {value: 'Class', label: 'Class'},
-        {value: 'Teacher', label: 'Teacher'},
-        {value: 'Student', label: 'Student'},
-        {value: 'Election', label: 'Election'}
+        { value: 'School', label: 'School' },
+        { value: 'Class', label: 'Class' },
+        { value: 'Teacher', label: 'Teacher' },
+        { value: 'Student', label: 'Student' },
+        { value: 'Election', label: 'Election' },
     ].filter((option) => {
         switch (currentRole) {
             case Role.Principal:
@@ -39,9 +32,11 @@ function CreationPage() {
             default:
                 return false;
         }
-    })
+    });
 
-    const [selectedOption, setSelectedOption] = useState<DropdownOption | undefined>(possibleCreateOptions[0]);
+    const [selectedOption, setSelectedOption] = useState<DropdownOption | undefined>(
+        possibleCreateOptions[0]
+    );
 
     const handleSelect = (value: string) => {
         const selected = possibleCreateOptions.find((option) => option.value === value);
@@ -55,7 +50,7 @@ function CreationPage() {
     function PossibleSelections() {
         return (
             <div>
-                <Dropdown options={possibleCreateOptions} onSelect={handleSelect}/>
+                <Dropdown options={possibleCreateOptions} onSelect={handleSelect} />
                 {renderSelectedOptionContent()}
             </div>
         );
@@ -68,26 +63,25 @@ function CreationPage() {
     const renderSelectedOptionContent = () => {
         switch (selectedOption?.value) {
             case 'School':
-                return <SchoolCreation/>;
+                return <SchoolCreation />;
             case 'Class':
-                return <ClassCreation/>;
+                return <ClassCreation />;
             case 'Teacher':
-                return <TeacherCreation/>;
+                return <TeacherCreation />;
             case 'Student':
-                return <StudentCreation/>;
+                return <StudentCreation />;
             case 'Election':
-                return <ElectionCreation/>
+                return <ElectionCreation />;
             default:
                 return null;
         }
     };
 
-
-    if (!isUserLoggedIn()) {
-        return <SignInInfoMessage/>
-    } else {
-        return PossibleSelections()
-    }
+    return (
+        <div className="creation-page-container"> {/* Apply the container class */}
+            {!isUserLoggedIn() ? <SignInInfoMessage /> : <PossibleSelections />}
+        </div>
+    );
 }
 
 export default CreationPage;
