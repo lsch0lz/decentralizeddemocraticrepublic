76c619249f872c763e1513dc6d6ea35132e48451
Lukas Scholz <lukas.scholz@deutschebahn.com>
2023-06-14 18:28:58 +0200

    added initial function for election

--- contracts/Schools.sol
+++ contracts/Schools.sol
@@ -1,73 +1,96 @@
 pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
 
 contract School {
-    
+
     struct Teacher {
         string name;
         uint256 id;
     }
-    
+
     struct Student {
         string name;
         string password;
         uint256 id;
     }
-    
+
     struct Class {
         string name;
         Teacher[] teachers;
         Student[] students;
     }
-    
+
     struct SchoolData {
         string name;
         address principal;
         mapping(uint256 => Class) classes;
     }
-    
-    mapping(address => SchoolData) public schools;  // Mapping to store school data (Principal is owner)
-    
-    function createSchool(string memory _name) public {
-        require(schools[msg.sender].principal == address(0), "School already exists");  // Check if school already exists
-        schools[msg.sender].name = _name;  // Set the school's name
-        schools[msg.sender].principal = msg.sender;  // Set the caller's address as the principal
+
+    struct Election {
+        string name;
+        string[] options;
+        uint voteCount;
     }
-    
+
+    Election[] public elections;
+
+    mapping(address => SchoolData) public schools;  // Mapping to store school data (Principal is owner)
+
+    function createSchool(string memory _name) public {
+        require(schools[msg.sender].principal == address(0), "School already exists");
+        // Check if school already exists
+        schools[msg.sender].name = _name;
+        // Set the school's name
+        schools[msg.sender].principal = msg.sender;
+        // Set the caller's address as the principal
+    }
+
     function createClass(uint256 _classId, string memory _name) public {
         SchoolData storage school = schools[msg.sender];
-        require(school.principal != address(0), "School does not exist");  // Check if the school exists
-        require(school.classes[_classId].teachers.length == 0, "Class already exists");  // Check if class already exists
-        school.classes[_classId].name = _name;  // Set the class name
+        require(school.principal != address(0), "School does not exist");
+        // Check if the school exists
+        require(school.classes[_classId].teachers.length == 0, "Class already exists");
+        // Check if class already exists
+        school.classes[_classId].name = _name;
+        // Set the class name
     }
-    
+
     function addTeacherToClass(uint256 _classId, string memory _teacherName, uint256 _teacherId) public {
         SchoolData storage school = schools[msg.sender];
-        require(school.principal != address(0), "School does not exist");  // Check if the school exists
-        require(school.classes[_classId].teachers.length > 0, "Class does not exist");  // Check if class exists
-        school.classes[_classId].teachers.push(Teacher(_teacherName, _teacherId));  // Add teacher to the class
+        require(school.principal != address(0), "School does not exist");
+        // Check if the school exists
+        require(school.classes[_classId].teachers.length > 0, "Class does not exist");
+        // Check if class exists
+        school.classes[_classId].teachers.push(Teacher(_teacherName, _teacherId));
+        // Add teacher to the class
     }
-    
+
     function addStudentToClass(uint256 _classId, string memory _studentName, uint256 _studentId, string memory _password) public {
         SchoolData storage school = schools[msg.sender];
-        require(school.principal != address(0), "School does not exist");  // Check if the school exists
-        require(school.classes[_classId].teachers.length > 0, "Class does not exist");  // Check if class exists
-        school.classes[_classId].students.push(Student(_studentName, _password, _studentId));  // Add student to the class
+        require(school.principal != address(0), "School does not exist");
+        // Check if the school exists
+        require(school.classes[_classId].teachers.length > 0, "Class does not exist");
+        // Check if class exists
+        school.classes[_classId].students.push(Student(_studentName, _password, _studentId));
+        // Add student to the class
     }
-    
+
     function getSchoolName() public view returns (string memory) {
-        return schools[msg.sender].name;  // Get the name of the school
+        return schools[msg.sender].name;
+        // Get the name of the school
     }
-    
+
     function getClassDetails(uint256 _classId) public view returns (string memory, uint256, uint256) {
         SchoolData storage school = schools[msg.sender];
         Class storage class = school.classes[_classId];
-        return (class.name, class.teachers.length, class.students.length);  // Get the class details (name, number of teachers, number of students)
+        return (class.name, class.teachers.length, class.students.length);
+        // Get the class details (name, number of teachers, number of students)
     }
 
     function log_in(string memory _studentName, string memory _password) public view returns (bool) {
         SchoolData storage school = schools[msg.sender];
         uint256 classCount = 0;
-        for (uint256 i = 0; i < 1000000; i++) { // Assuming the maximum number of classes is 1,000,000
+        for (uint256 i = 0; i < 1000000; i++) {// Assuming the maximum number of classes is 1,000,000
             if (school.classes[i].teachers.length > 0) {
                 classCount++;
             } else {
@@ -82,11 +105,32 @@ contract School {
                     keccak256(bytes(students[j].name)) == keccak256(bytes(_studentName)) &&
                     keccak256(bytes(students[j].password)) == keccak256(bytes(_password))
                 ) {
-                    return true;  // Found the student
+                    return true;
+                    // Found the student
                 }
             }
         }
 
-        return false;  // Student not found
+        return false;
+        // Student not found
     }
+
+    function createElection(string memory electionName, string[] memory electionOptions) public {
+        // Create an election
+        elections.push(Election(electionName, electionOptions, 0));
+
+    }
+
+    function getElection(string memory electionName) public view returns (string memory, string[] memory, uint) {
+        for (uint i = 0; i < elections.length; i++) {
+            if (keccak256(bytes(elections[i].name)) == keccak256(bytes(electionName))) {
+                require(i < elections.length, "Invalid index");
+
+                Election storage election = elections[i];
+                return (election.name, election.options, election.voteCount);
+            }
+        }
+        revert("Election not found");
+    }
+
 }
--- webclient/src/components/CreationPage/creationforms/ElectionCreation.tsx
+++ webclient/src/components/CreationPage/creationforms/ElectionCreation.tsx
@@ -1,9 +1,44 @@
-import React, { useState } from 'react';
-import { CustomFormLabel } from '../customInput/CustomFormLabel';
+import React, {useState} from 'react';
+import {CustomFormLabel} from '../customInput/CustomFormLabel';
 import './ElectionCreation.css';
+import schoolContract from "../../../contracts/School.json";
+import Web3 from "web3";
+
+
+const contractABI = schoolContract.abi;
+const contractAddress = '0x1935f22D4803f78A01A0B5E5aBf6BaC7719effD7'; // Replace with your contract address
+
+const ganacheUrl = 'HTTP://127.0.0.1:7545';
+const httpProvider = new Web3.providers.HttpProvider(ganacheUrl);
+const web3 = new Web3(httpProvider);
+
+// @ts-ignore
+const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
 
 interface OptionsFormProps {
     onSubmit: (options: string[]) => void;
+}
+
+const createElection = async (electionName: string, options: string[]) => {
+    const accounts = await web3.eth.getAccounts();
+    try {
+        console.log('Creating election with name:', electionName)
+        console.log('Creating election with options:', options)
+        return await contractInstance.methods.createElection(electionName, options).send({from: accounts[0]});
+    } catch (error) {
+        console.error('Failed to create election:', error);
+    }
+}
+
+const getElection = async (electionName: string) => {
+    try {
+        console.log('Getting election with name:', electionName)
+        const result = await contractInstance.methods.getElection(electionName).call()
+        return result;
+    } catch (error) {
+        console.error('Failed to get election:', error);
+    }
 }
 
 export function ElectionCreation() {
@@ -32,10 +67,21 @@ export function ElectionCreation() {
         }
     };
 
-    const saveToChain = (e: React.FormEvent) => {
+    const saveToChain = async (e: React.FormEvent) => {
         e.preventDefault();
         // TODO: Save to chain
-        console.log('Saving to chain! Need to be implemented');
+        console.log('Saving election to chain with name:', electionName)
+        console.log('Saving election to chain with options:', optionsList)
+
+        await createElection(electionName, optionsList);
+        console.log("Saved election to chain", electionName)
+        console.log("Getting election from chain", electionName)
+        const result = await getElection(electionName);
+        if (result && result[0]) {
+            console.log('Got election from chain', result);
+        } else {
+            console.log('Election not found');
+        }
     };
 
     return (
