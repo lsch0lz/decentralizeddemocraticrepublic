b10bee25c14df89e2c9a5f6d418a28f4a6a73510
Moritz Lindner <lindner197@gmail.com>
2023-06-20 11:22:12 +0200

    using new method in election

--- webclient/src/components/CreationPage/creationforms/ElectionCreation.tsx
+++ webclient/src/components/CreationPage/creationforms/ElectionCreation.tsx
@@ -1,54 +1,10 @@
 import React, {useState} from 'react';
 import {CustomFormLabel} from '../customInput/CustomFormLabel';
 import './ElectionCreation.css';
-import schoolContract from "../../../contracts/School.json";
-import Web3 from "web3";
-
-
-const contractABI = schoolContract.abi;
-const contractAddress = '0x1935f22D4803f78A01A0B5E5aBf6BaC7719effD7'; // Replace with your contract address
-
-const ganacheUrl = 'HTTP://127.0.0.1:7545';
-const httpProvider = new Web3.providers.HttpProvider(ganacheUrl);
-const web3 = new Web3(httpProvider);
-
-// @ts-ignore
-const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
-
+import {createElection, getWinner, vote} from "SchoolContract";
 
 interface OptionsFormProps {
     onSubmit: (options: string[]) => void;
-}
-
-const createElection = async (electionName: string, options: string[]) => {
-    const accounts = await web3.eth.getAccounts();
-    try {
-        console.log('Creating election with name:', electionName)
-        console.log('Creating election with options:', options)
-        return await contractInstance.methods.createElection(electionName, options).send({from: accounts[0]});
-    } catch (error) {
-        console.error('Failed to create election:', error);
-    }
-}
-
-const getVotes = async (electionID: number) => {
-    try {
-        console.log('Getting votes for election with ID:', electionID)
-        const result = await contractInstance.methods.getVotes(electionID).call()
-        return result;
-    } catch (error) {
-        console.error('Failed to get votes:', error);
-    }
-}
-
-const vote = async (electionID: number) => {
-    try {
-        console.log('Voting for election with ID:', electionID)
-        const result = await contractInstance.methods.vote(electionID).call()
-        return result;
-    } catch (error) {
-        console.error('Failed to vote:', error);
-    }
 }
 
 export function ElectionCreation() {
@@ -83,44 +39,49 @@ export function ElectionCreation() {
         console.log('Saving election to chain with name:', electionName)
         console.log('Saving election to chain with options:', optionsList)
 
-        await createElection(electionName, optionsList);
+        // TODO: Fill with correct values
+        await createElection(1, electionName, optionsList, "");
         console.log("Saved election to chain", electionName)
         console.log("Getting election from chain", electionName)
-        await vote(1);
-        const result = await getVotes(1);
+
+        // TODO: Fill with correct values
+        await vote(1, "1", "")
+
+        // TODO: Fill with correct values
+        const result = await getWinner(1, "");
         console.log("Got election from chain", result)
     };
 
-        return (
-            <div className="ElectionCreation">
-                <form onSubmit={saveToChain}>
-                    <CustomFormLabel
-                        label="Election Name"
-                        type="text"
-                        value={electionName}
-                        onChange={handleElectionNameChange}
-                    />
-                    <CustomFormLabel
-                        label="Add Option"
-                        type="text"
-                        value={option}
-                        onChange={handleOptionChange}
-                    />
-                    <button type="button" onClick={handleAddOption}>
-                        Add Option
-                    </button>
-                    <ul>
-                        {optionsList.map((opt, index) => (
-                            <li key={index}>
-                                {opt}
-                                <button type="button" onClick={() => handleRemoveOption(index)}>
-                                    Remove
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                    <button type="submit">Create Election</button>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="ElectionCreation">
+            <form onSubmit={saveToChain}>
+                <CustomFormLabel
+                    label="Election Name"
+                    type="text"
+                    value={electionName}
+                    onChange={handleElectionNameChange}
+                />
+                <CustomFormLabel
+                    label="Add Option"
+                    type="text"
+                    value={option}
+                    onChange={handleOptionChange}
+                />
+                <button type="button" onClick={handleAddOption}>
+                    Add Option
+                </button>
+                <ul>
+                    {optionsList.map((opt, index) => (
+                        <li key={index}>
+                            {opt}
+                            <button type="button" onClick={() => handleRemoveOption(index)}>
+                                Remove
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+                <button type="submit">Create Election</button>
+            </form>
+        </div>
+    );
+}
