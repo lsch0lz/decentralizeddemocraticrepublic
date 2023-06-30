dae29db0b2c7f0299fa32addd2366310e57ac224
lsch0olz <lscholz14@googlemail.com>
2023-06-30 12:49:54 +0200

    get elections and winner

--- webclient/src/components/NewsPage/NewsPage.tsx
+++ webclient/src/components/NewsPage/NewsPage.tsx
@@ -3,26 +3,43 @@ import SignInInfoMessage from "../SignInInfoMessage";
 import {getAllElectionIDs, getElectionName, getElectionWinner, getOptionsFromElection} from "../Contract";
 import RoleContext from "../RoleContext";
 
-function NewsPage(){
+function NewsPage() {
     const {currentRole} = useContext(RoleContext);
-    function isUserLoggedIn() {
-        return currentRole !== null;
+
+    async function showWinner(index: number) {
+        // Show all elections and the current winner
+        const currentElectionIds = await getAllElectionIDs("JMG");
+        const electionNames = await Promise.all(currentElectionIds.map((id: string) => getElectionName(id, "JMG")));
+        const electionWinners = await Promise.all(currentElectionIds.map((id: string) => getElectionWinner(id, "JMG")));
+
+        return electionNames.map((name, index) => ({
+            label: electionNames[index],
+            winner: electionWinners[index]
+        }));
     }
 
-    function PossibleNewsPage(){
-        return (
-            <div>
-                <h1>News Page</h1>
-                <p>Hier könnt ihr die neuesten Abstimmungen und ihre Gewinner sehen!</p>
-            </div>
-        );
-    }
+
+
+function isUserLoggedIn() {
+    return currentRole !== null;
+}
+
+function PossibleNewsPage() {
     return (
-        <div className="creation-page-container"> {/* Apply the container class */}
-            {!isUserLoggedIn() ? <SignInInfoMessage/> : <PossibleNewsPage/>}
+        <div>
+            <h1>News Page</h1>
+            <p>Hier könnt ihr die neuesten Abstimmungen und ihre Gewinner sehen!</p>
+            <button onClick={() => showWinner(0)}>Show Winner</button>
+
         </div>
     );
+}
 
+return (
+    <div className="creation-page-container"> {/* Apply the container class */}
+        {!isUserLoggedIn() ? <SignInInfoMessage/> : <PossibleNewsPage/>}
+    </div>
+    );
 }
 
 
