d4259e712b90c321cae98dc9bff38b69fe477f70
lsch0olz <lscholz14@googlemail.com>
2023-06-30 13:45:24 +0200

    added more text for home

--- webclient/src/components/Home.tsx
+++ webclient/src/components/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Home page body content</p>
+        <div style={{
+            textAlign: "center",
+        }}>
+            <h1>Decentralized Democratic Republic</h1>
+            <p>Herzlich Willkommen auf der Homepage deiner demokratischen Schule!</p>
+            <p>Bitte logge dich ein, um Abstimmen zu können oder die weiteren Funktionen zu nutzen.</p>
         </div>
     );
 }
