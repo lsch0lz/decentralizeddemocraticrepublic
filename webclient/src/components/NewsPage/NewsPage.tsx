f9e51f03f0d8355a8864f8968150923a9d4deb61
lsch0olz <lscholz14@googlemail.com>
2023-06-30 12:29:12 +0200

    added login check

--- webclient/src/components/NewsPage/NewsPage.tsx
+++ webclient/src/components/NewsPage/NewsPage.tsx
@@ -1,15 +1,28 @@
 import React, {useContext, useEffect, useState} from 'react';
 import SignInInfoMessage from "../SignInInfoMessage";
 import {getAllElectionIDs, getElectionName, getElectionWinner, getOptionsFromElection} from "../Contract";
+import RoleContext from "../RoleContext";
 
 function NewsPage(){
+    const {currentRole} = useContext(RoleContext);
+    function isUserLoggedIn() {
+        return currentRole !== null;
+    }
 
+    function PossibleNewsPage(){
+        return (
+            <div>
+                <h1>News Page</h1>
+                <p>Hier könnt ihr die neuesten Abstimmungen und ihre Gewinner sehen!</p>
+            </div>
+        );
+    }
     return (
-        <div>
-            <h1>News Page</h1>
-            <p>This is a test</p>
+        <div className="creation-page-container"> {/* Apply the container class */}
+            {!isUserLoggedIn() ? <SignInInfoMessage/> : <PossibleNewsPage/>}
         </div>
     );
+
 }
 
 
