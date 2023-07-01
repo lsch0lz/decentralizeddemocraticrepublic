122968c330f8134de3b7e64043eb35f3967ac6a9
Moritz Lindner <lindner197@gmail.com>
2023-07-01 11:19:53 +0200

    implement rendering of election results

--- webclient/src/components/NewsPage/NewsPage.tsx
+++ webclient/src/components/NewsPage/NewsPage.tsx
@@ -1,45 +1,71 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useState} from 'react';
 import SignInInfoMessage from "../SignInInfoMessage";
-import {getAllElectionIDs, getElectionName, getElectionWinner, getOptionsFromElection} from "../Contract";
+import {getAllElectionIDs, getElectionName, getElectionWinner} from "../Contract";
 import RoleContext from "../RoleContext";
+
+export interface ElectionResult {
+    value: string;
+    label: string;
+}
+
 
 function NewsPage() {
     const {currentRole} = useContext(RoleContext);
+    const [electionResults, setElectionResults] = useState<ElectionResult[]>([]);
 
-    async function showWinner(index: number) {
+
+    async function showWinner() {
         // Show all elections and the current winner
         const currentElectionIds = await getAllElectionIDs("JMG");
         const electionNames = await Promise.all(currentElectionIds.map((id: string) => getElectionName(id, "JMG")));
         const electionWinners = await Promise.all(currentElectionIds.map((id: string) => getElectionWinner(id, "JMG")));
 
-        return electionNames.map((name, index) => ({
-            label: electionNames[index],
-            winner: electionWinners[index]
+        let results = electionNames.map((name, index) => ({
+            value: electionWinners[index], // Set the value as the winner
+            label: name, // Set the label as the election name
         }));
+
+        setElectionResults(results);
+    }
+
+    function isUserLoggedIn() {
+        return currentRole !== null;
     }
 
 
 
-function isUserLoggedIn() {
-    return currentRole !== null;
-}
+    function PossibleNewsPage() {
+        return (
+            <div>
+                <h1>News Page</h1>
+                <p>Hier könnt ihr die neuesten Abstimmungen und ihre Gewinner sehen!</p>
+                <button onClick={() => showWinner()}>Show Winner</button>
 
-function PossibleNewsPage() {
+                {electionResults.map((result, index) => (
+                    <RenderElectionResult key={index} electionResult={result} />
+                ))}
+
+            </div>
+        );
+    }
+
     return (
-        <div>
-            <h1>News Page</h1>
-            <p>Hier könnt ihr die neuesten Abstimmungen und ihre Gewinner sehen!</p>
-            <button onClick={() => showWinner(0)}>Show Winner</button>
-
+        <div className="creation-page-container"> {/* Apply the container class */}
+            {!isUserLoggedIn() ? <SignInInfoMessage/> : <PossibleNewsPage/>}
         </div>
     );
 }
 
-return (
-    <div className="creation-page-container"> {/* Apply the container class */}
-        {!isUserLoggedIn() ? <SignInInfoMessage/> : <PossibleNewsPage/>}
-    </div>
-    );
+interface RenderElectionResultProps {
+    electionResult: ElectionResult
+}
+
+function RenderElectionResult(props: RenderElectionResultProps) {
+    let props1 = props;
+
+    return (
+        <p>{props1.electionResult.label}</p>
+    )
 }
 
 
